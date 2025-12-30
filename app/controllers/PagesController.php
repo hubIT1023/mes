f5674f0ba248d2f623781a2d493e851606bf2127
hubIT1023 <hubITtoday@gmail.com>
2025-12-31 03:09:15 +1300

    most correction update

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -1,16 +1,17 @@
 <?php
+// PagesController.php
+
+// Require the BASE Controller class (not this file!)
 require_once __DIR__ . '/Controller.php';
 
 class PagesController extends Controller {
-
     public function welcome() {
         $this->view('welcome_page', [
             'title' => 'Welcome to hubIT.online',
             'tagline' => 'Your reliable IT partner for digital solutions.'
         ]);
     }
-	
-	
+
     public function about() {
         $this->view('about', [
             'title' => 'About Us',
@@ -25,25 +26,24 @@ class PagesController extends Controller {
     public function register() {
         $this->view('register', ['title' => 'Register']);
     }
-	
-	public function canvas() {
+
+    public function canvas() {
         $this->view('canvas', ['title' => 'Canvas']);
     }
-	
-	public function mms_admin() {
+
+    public function mms_admin() {
         $this->view('mms_admin', ['title' => 'mms_Admin']);
     }
-	
-	public function dashboard_admin() {
+
+    public function dashboard_admin() {
         $this->view('dashboard_admin', ['title' => 'Dashboard_Admin']);
     }
-	
-	
-	public function demo_dashboard() {
+
+    public function demo_dashboard() {
         $this->view('demo/demo_dashboard', ['title' => 'Demo_Dashboard']);
     }
 
-	public function demo_mes() {
+    public function demo_mes() {
         $this->view('demo/demo_mes', ['title' => 'Demo_MES']);
     }
 }
