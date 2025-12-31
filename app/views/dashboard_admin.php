7047ae019161782a19c18ff4e8346aa82a63fea7
hubIT1023 <hubITtoday@gmail.com>
2026-01-01 00:56:46 +1300

    update favicon

--- app/views/dashboard_admin.php
+++ app/views/dashboard_admin.php
@@ -122,7 +122,7 @@ function base_url($path = '') {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>HubIT Dashboard</title>
-<link rel="icon" type="image/png" href="/Assets/img/favicon.png">
+<link rel="icon" type="image/png" href="/app/Assets/img/favicon.png">
 <script src="https://cdn.tailwindcss.com"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
