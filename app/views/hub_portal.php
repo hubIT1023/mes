e820ced156bbf654b6f4180a9ca8510c6bd6917c
hubIT1023 <hubITtoday@gmail.com>
2025-12-31 21:40:47 +1300

    update favicon path

--- app/views/hub_portal.php
+++ app/views/hub_portal.php
@@ -23,8 +23,8 @@ $tenant = $_SESSION['tenant'];
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= htmlspecialchars($tenant['org_name'] ?? 'HubIT.online') ?> - Enterprise Portal</title>
 
-  <link rel="icon" type="image/png" href="/../../Assets/images/favicon.png">
-
+ <link rel="icon" type="image/png" href="/Assets/img/favicon.png">
+ 
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
   <link href="../assets/css/hub_portal.css" rel="stylesheet">
