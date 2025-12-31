0a9808dec230bb8f62ccb92ba8bec94daaa079e1
hubIT1023 <hubITtoday@gmail.com>
2025-12-31 21:27:50 +1300

    add favcon

--- app/views/hub_portal.php
+++ app/views/hub_portal.php
@@ -23,10 +23,11 @@ $tenant = $_SESSION['tenant'];
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= htmlspecialchars($tenant['org_name'] ?? 'HubIT.online') ?> - Enterprise Portal</title>
 
+  <link rel="icon" type="image/png" href="../assets/images/favicon.png">
+
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
   <link href="../assets/css/hub_portal.css" rel="stylesheet">
- 
 </head>
 
 <body class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen flex flex-col">
