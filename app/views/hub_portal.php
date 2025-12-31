2b49c9b91422739465ae230c102922c1f446e40e
hubIT1023 <hubITtoday@gmail.com>
2025-12-31 21:49:58 +1300

    update

--- app/views/hub_portal.php
+++ app/views/hub_portal.php
@@ -23,7 +23,7 @@ $tenant = $_SESSION['tenant'];
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= htmlspecialchars($tenant['org_name'] ?? 'HubIT.online') ?> - Enterprise Portal</title>
 
- <link rel="icon" type="image/png" href="/Assets/img/favicon.png">
+ <link rel="icon" type="image/png" href="/Assets/img/favicon.png?v=3">
  
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
