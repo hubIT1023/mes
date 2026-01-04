bb193f1873f7885564ea0d7cb3c35327dfcdcd31
hubIT1023 <hubITtoday@gmail.com>
2026-01-04 15:46:16 +1300

    update

--- app/views/utilities/entity_toolState_card.php
+++ app/views/utilities/entity_toolState_card.php
@@ -312,7 +312,7 @@ $csrfToken = $_SESSION['csrf_token'] ?? '';
     <div class="modal-dialog modal-sm modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h6 class="modal-title" id="setMaintModalLabel">More Actions</h6>
+                <h6 class="modal-title" id="setMaintModalLabel">SELECT ACTION</h6>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body p-0">
