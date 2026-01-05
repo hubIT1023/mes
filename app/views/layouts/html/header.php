38469d213903de7e6e8fc1a8bebfe48ca0a944c1
hubIT1023 <hubITtoday@gmail.com>
2026-01-06 01:19:53 +1300

    update

--- app/views/layouts/html/header.php
+++ app/views/layouts/html/header.php
@@ -11,7 +11,9 @@
 
   <!-- Bootstrap 5 CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-
+<script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <style>
   <!-- Your other styles -->
   <link href="../Assets/css/sb-admin-2.min.css" rel="stylesheet" />
   <link href="../Assets/css/custom-colors.css" rel="stylesheet"  />
