6ab36323a38f605cdeecdda258ee29e06f299ddd
hubIT1023 <hubITtoday@gmail.com>
2026-01-09 10:15:49 +1300

    update

--- app/views/devices/list_devices.php
+++ app/views/devices/list_devices.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/../layouts/html/header.php'; ?>
 
 <div class="container-lg mt-4">
-    <div class="d-flex justify-content-between gap-2 align-items-center mb-4">
+    <div class="d-flex justify-content-between gap-2 mb-4">
         <div>
             <h2 class="fw-bold">Registered Devices</h2>
             <p class="text-muted mb-0">Manage your connected devices</p>
