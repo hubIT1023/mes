0ae884ebe81ec898416d306c1290baf5f45d1768
hubIT1023 <hubITtoday@gmail.com>
2026-01-10 23:52:35 +1300

    update

--- app/models/ToolStateModel.php
+++ app/models/ToolStateModel.php
@@ -32,42 +32,6 @@ class ToolStateModel {
     $stmt->execute($data);
 }
 			 
-	//process the data
-	
-	public function processToolStateData(array $data): void
-{
-    $stmt = $this->conn->prepare("
-        UPDATE tool_state
-        SET
-            col_10 = col_3,
-            col_7  = NOW()
-        WHERE org_id = :org_id
-          AND col_1  = :col_1
-    ");
-
-    $stmt->execute($data);
-}
-			 
-	//save data to history
-	public function saveHistoryToMachineLog(array $data): void
-{
-    $stmt = $this->conn->prepare("
-        INSERT INTO machine_log (
-            org_id, group_code, location_code,
-            col_1, col_2, col_3, col_4, col_5,
-            col_6, col_7, col_8, col_9, col_10
-        )
-        SELECT
-            org_id, group_code, location_code,
-            col_1, col_2, col_3, col_4, col_5,
-            col_6, NOW(), col_8, col_9, col_10
-        FROM tool_state
-        WHERE org_id = :org_id
-          AND col_1  = :col_1
-    ");
-
-    $stmt->execute($data);
-}
 
     
 	// custom stopcause (IDELE,PROD,MAINT...)
